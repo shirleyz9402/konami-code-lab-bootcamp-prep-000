d66ac662c54c4ce44e0ba0db6294b3a03d97d064
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-04 21:28:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,4 +3,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
 
+function onKeyDownHandler(e) {
+  var arr = []
+  const key = parseInt(e.which);
+  for (let i = 0; i < code.length - 1; i++) {
+  if (key === code[i]) {
+    arr.push(key)
+    i++
+    }
+  else {
+    i = 0
+    }  
+  }
+  if (arr === code) {
+    window.alert("Hurray!")
+    
+  }
+}
 }
